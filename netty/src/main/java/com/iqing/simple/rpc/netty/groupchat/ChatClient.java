aca835e38564ff19b4da6e4598a1031d2c094fb4
爱青 <aiqing.wyx@antgroup.com>
2021-01-25 19:49:03 +0800

    add groupchat version 1

--- netty/src/main/java/com/iqing/simple/rpc/netty/groupchat/ChatClient.java
+++ netty/src/main/java/com/iqing/simple/rpc/netty/groupchat/ChatClient.java
@@ -4,9 +4,103 @@
  */
 package com.iqing.simple.rpc.netty.groupchat;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * @author aiqing.wyx
  * @version ChatClient.java, v 0.1 2021年01月24日 8:20 下午 wangyuxiang Exp $
  */
 public class ChatClient {
+
+    private Selector selector;
+
+    private SocketChannel socketChannel;
+
+    private static final String IP = "127.0.0.1";
+
+    private static final int PORT = 9999;
+
+    public ChatClient() {
+        try {
+            selector = Selector.open();
+            socketChannel = SocketChannel.open();
+            Socket socket = socketChannel.socket();
+            socket.connect(new InetSocketAddress(IP, PORT));
+            socketChannel.configureBlocking(false);
+            socketChannel.register(selector, SelectionKey.OP_READ);
+        } catch (IOException e) {
+            System.out.println("client 启动失败！！！");
+        }
+    }
+
+    public void sendMsg(String msg) {
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(msg.getBytes());
+        try {
+            socketChannel.write(byteBuffer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void receiveMsg() {
+        try {
+            int count = selector.select(2000);
+
+            if (count > 0) {
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
+                while (iterator.hasNext()) {
+                    SelectionKey selectionKey = iterator.next();
+
+                    if (selectionKey.isReadable()) {
+
+                        SocketChannel channel = (SocketChannel) selectionKey.channel();
+
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+                        int read = channel.read(byteBuffer);
+
+                        if (read > 0) {
+                            System.out.println("收到数据");
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("未知异常");
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatClient chatClient = new ChatClient();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    chatClient.receiveMsg();
+                }
+            }
+        }).start();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String msg = sc.nextLine();
+            chatClient.sendMsg(msg);
+        }
+    }
 }
--- netty/src/main/java/com/iqing/simple/rpc/netty/groupchat/ChatServer.java
+++ netty/src/main/java/com/iqing/simple/rpc/netty/groupchat/ChatServer.java
@@ -4,9 +4,124 @@
  */
 package com.iqing.simple.rpc.netty.groupchat;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
+
 /**
  * @author aiqing.wyx
  * @version ChatServer.java, v 0.1 2021年01月24日 8:20 下午 wangyuxiang Exp $
  */
 public class ChatServer {
+
+    private ServerSocketChannel listenChannel;
+
+    private Selector selector;
+
+    private static final int PORT = 9999;
+
+    public ChatServer() {
+        try {
+            this.listenChannel = ServerSocketChannel.open();
+
+            this.selector = Selector.open();
+
+            listenChannel.socket().bind(new InetSocketAddress(PORT));
+
+            listenChannel.configureBlocking(false);
+
+            listenChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        } catch (IOException e) {
+            System.out.println("ChatServer 启动失败！！！");
+        }
+    }
+
+    public void listen() {
+
+        while (true) {
+            try {
+                int count = selector.select(2000);
+
+                if (count > 0) {
+                    Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+                    Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey selectionKey = iterator.next();
+
+                        // 建立连接
+                        if (selectionKey.isAcceptable()) {
+
+                            SocketChannel socketChannel = listenChannel.accept();
+
+                            socketChannel.configureBlocking(false);
+
+                            socketChannel.register(selector, SelectionKey.OP_READ);
+
+                            System.out.println(socketChannel.getRemoteAddress() + "客户端连接了");
+                        }
+                        // 读数据
+                        if (selectionKey.isReadable()) {
+
+                            SocketChannel channel = null;
+                            try {
+                                channel = (SocketChannel) selectionKey.channel();
+
+                                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+                                int read = channel.read(byteBuffer);
+
+                                if(read > 0) {
+                                    String msg = new String(byteBuffer.array());
+
+                                    //往其他客户端转发消息
+                                    SocketChannel finalChannel = channel;
+                                    selectionKeys.stream()
+                                            .map(SelectionKey::channel)
+                                            .filter(selectableChannel -> finalChannel != selectableChannel)
+                                            .map(selectableChannel -> (SocketChannel)selectableChannel)
+                                            .filter(Objects::nonNull)
+                                            .forEach(
+                                                    socketChannel -> {
+                                                        ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+                                                        try {
+                                                            socketChannel.write(buffer);
+                                                        } catch (IOException e) {
+                                                            e.printStackTrace();
+                                                        }
+                                                    }
+                                            );
+                                }
+                            }catch (IOException e) {
+                                System.out.println(channel.getRemoteAddress() + "下线了");
+
+                            }
+
+                        }
+
+                    }
+
+                } else {
+                    System.out.println("等待客户端连接。。。");
+                }
+            } catch (IOException e) {
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatServer chatServer = new ChatServer();
+
+        chatServer.listen();
+    }
 }
